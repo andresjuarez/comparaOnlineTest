b6e371181ef70d1fce383efa6d9b648839e03873
andresjuarez <andresalejandro18@gmail.com>
2018-12-31 10:19:45 -0300

    Creating updatePrice

--- src/carInsurance.js
+++ src/carInsurance.js
@@ -1,5 +1,11 @@
+
 export default class CarInsurance {
   constructor (products = []) {
     this.products = products
+  }
+  updatePrice () {
+    this.products.map((product) => {
+      console.log(product)
+    })
   }
 }
